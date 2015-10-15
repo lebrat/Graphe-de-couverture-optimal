2f1c25efd9f493d90673b323d1dd08d0ab739d2a
Léo Lebrat <leo.lebrat@gmail.com>
2015-10-15 11:01:04 +0200

    Add a representation model for graph

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,20 +1,48 @@
+import java.util.Random;
+
 import grph.Grph;
 
 public class Graphe {
-	private int nb_sommet;
+	private static int nb_sommet;
 	private int nb_generateur;
 	private int[] generateur = new int[nb_generateur];
 	private int[] position = new int[nb_generateur];
+	private static int [][] connectivite;
+	
+	public static void init(int nb_s,int borne_sup, float densite){
+		nb_sommet = nb_s;
+		Random r = new Random();
+		connectivite = new int[nb_sommet][nb_sommet];
+		for(int i = 0 ; i < nb_sommet ; i++){
+			for(int j = i; j < nb_sommet; j++){
+				if(r.nextInt(100)<densite){
+					connectivite[i][j] = r.nextInt(borne_sup);
+					connectivite[j][i] = connectivite[i][j];
+				}
+				else{
+					connectivite[i][j] = 0;
+					connectivite[j][i] = 0;
+				}
+			}
+		}		
+	}
+	
+	public static void print(){
+		Grph g = new grph.in_memory.InMemoryGrph();
+		g.addNVertices(nb_sommet);
+		for(int i = 0; i < nb_sommet-1; i++){
+			for(int j = i+1; j < nb_sommet; j++){
+				if (connectivite[i][j] > 0){
+					g.addUndirectedSimpleEdge(i, j);
+				}
+			}
+		}
+		g.display();
+	}
 	
 	public static void main(String[] args)
     {
-	Grph g = new grph.in_memory.InMemoryGrph();
-	g.display();
-	g.grid(10, 10);
-	g.getDiameter();
-	g.removeVertex(11);
-	g.removeVertex(21);
-	/*p = g.getShortestPath(0, 54)
-	p.setColor(g, 5)*/
+	init(20,10,50);
+	print();
     }
 }
